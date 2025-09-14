de0c945a710d71f7d810e4b949a54f63b4a388ac
Javier Ignacio Riquelme Torres <gospinforma@gmail.com>
2025-09-14 15:26:41 -0300

    feat: Se diseño el contenido de contacto.html, y se añadio un formulario externo (Formspree)

--- JS/master.js
+++ JS/master.js
@@ -5,3 +5,27 @@ fetch("navbar.html")
 fetch("footer.html")
   .then(res => res.text())
   .then(data => document.getElementById("footer-placeholder").innerHTML = data);
+
+/*AREA DE CONTACTO*/
+const form = document.getElementById('contactForm');
+const successMessage = document.getElementById('successMessage');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); // evita recarga
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+    }).then(response => {
+        if (response.ok) {
+            successMessage.style.display = 'block';
+            form.reset();
+        } else {
+            alert('Ocurrió un error al enviar el mensaje.');
+        }
+    }).catch(error => {
+        alert('Error de conexión: ' + error);
+    });
+});
