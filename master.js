dc334ea5c492ee1e8de2d32e2f4b8ba0b4124046
Javier Ignacio Riquelme Torres <gospinforma@gmail.com>
2025-09-13 23:37:05 -0300

    feat: master.js created, and replace the master-page functionality

---
+++ master.js
@@ -0,0 +1,7 @@
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("navbar-placeholder").innerHTML = data);
+
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("footer-placeholder").innerHTML = data);
